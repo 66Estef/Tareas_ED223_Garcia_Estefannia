45eb427596712f4a6255f22c855a08d0086cac53
66Estef <lintzyzep@gmail.com>
2022-06-07 19:02:50 -0500

    Corrección Tarea Quizz listo

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,31 +5,32 @@ using TMPro;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
     public TextMeshProUGUI texto;
     // Start is called before the first frame update
     void Start()
     {
-        texto.tex = "Caricatura favorita";
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.1))
+        if(Input.GetKeyDown(KeyCode.M))
         {
-            texto.text = "Yo iré solo por que soy BÁRBARO";
+            texto.text="Yo iré solo porque soy BÁRBARO";
         }
         if(Input.GetKeyDown(KeyCode.H))
         {
-            texto.text = "Apártate cabeza de balón";
+            texto.text="¡Apártate cabeza de balón!";
         }
         if(Input.GetKeyDown(KeyCode.B))
         {
-            texto.text = "¡Y así nacieron las chicas superrrrrrpoderosas";
+            texto.text="Y así nacieron las chicas superrrrrrpoderosas:Bombón, Burrrbuja y Bellota";
         }
         if(Input.GetKeyDown(KeyCode.P))
         {
-            texto.text = "Ferb, ¡ya se que vamos a hacer hoy!";
+            texto.text="Ferb,¡ya se que vamos a hacer hoy!";
         }
     }
-}
+}// fin del cuerpo de class
